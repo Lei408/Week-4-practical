c1a8f27318fa9f2c64ca1330ee70ea0d8b4b76b7
Lei408 <116322348+Lei408@users.noreply.github.com>
2023-04-08 21:47:36 +0800

    Add files via upload

---
+++ week 4 practical/week 4 practical/week 4 practical.cpp
@@ -0,0 +1,48 @@
+﻿#include<set>
+#include<string>
+#include <iostream>
+#include<map>
+int main()
+{
+	std::set<int>Number;
+
+	int amount;
+	std::cout << "How many numbers do you want to enter?";
+	std::cin >> amount;
+
+	int x;
+	for (int i = 0; i < amount; i++) {
+		std::cout << "Enter a number:";
+		std::cin >> x;
+		Number.insert(x);
+	}
+
+		int Check ;
+		std::cout << "Please enter a number to check : \n";
+		std::cin >> Check;
+
+		if (Number.count(Check) == 0) {
+			std::cout << "Not found\n";
+		};
+		if (Number.count(Check) == 1) {
+			std::cout << "Found it\n";
+		};
+
+		int Erase;
+		std::cout << "Enter a number to erase:\n";
+		std::cin >> Erase;
+
+
+		if (Number.count(Erase) == 0) {
+			std::cout << "Not found\n";
+		};
+
+		if (Number.count(Erase) == 1) {
+			Number.erase(Erase);
+			std::cout << "Erase it\n";
+		
+		};
+
+		std::map<std::string, int>Map;
+		std::string MapString;
+	}
